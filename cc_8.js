d56711781202779a717f732a3fe72f01aefc1703
Gavrie Grant <gavrie@usf.edu>
2025-02-16 11:05:52 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -1,4 +1,4 @@
-// Task 1  Function Declaration 
+
 // Task 1  Function Declaration 
 
 function calculateSalary(baseSalary, bonus, taxRate) {
@@ -11,3 +11,20 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 // Test Data:
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary: $6950.00"
+
+
+
+
+
+//Task 2 Product Price After Discount 
+const calculateDiscount = function(price, discountRate) {
+    // Calculate the final price after discount
+    const finalPrice = price - (price * discountRate);
+    // Return the final price formatted as a string
+    return "Final Price: $" + finalPrice.toFixed(2);
+  };
+  
+// Test Data:
+console.log(calculateDiscount(100, 0.2)); 
+console.log(calculateDiscount(250, 0.15)); 
+
